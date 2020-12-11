68e95939d3497b679b2c20f204697c53e7015800
Meghan <hello@nektro.net>
2020-12-11 13:22:23 -0800

    update go version

--- build_all.sh
+++ build_all.sh
@@ -12,22 +12,31 @@ build_template() {
     go build -ldflags="-s -w -X main.Version=$tag" -o ./bin/comics-dl-$tag-$GOOS-$GOARCH$ext
 }
 
+build_template aix ppc64
+# build_template android 386
+# build_template android amd64
+# build_template android arm
+# build_template android arm64
 build_template darwin 386
 build_template darwin amd64
+# build_template darwin arm
+# build_template darwin arm64
 build_template dragonfly amd64
 build_template freebsd 386
 build_template freebsd amd64
 build_template freebsd arm
+build_template illumos amd64
+# build_template js wasm
 build_template linux 386
 build_template linux amd64
 build_template linux arm
 build_template linux arm64
-build_template linux ppc64
-build_template linux ppc64le
 build_template linux mips
-build_template linux mipsle
 build_template linux mips64
 build_template linux mips64le
+build_template linux mipsle
+build_template linux ppc64
+build_template linux ppc64le
 build_template linux s390x
 # build_template nacl 386
 # build_template nacl amd64p32
@@ -35,12 +44,15 @@ build_template linux s390x
 build_template netbsd 386
 build_template netbsd amd64
 build_template netbsd arm
+build_template netbsd arm64
 build_template openbsd 386
 build_template openbsd amd64
 build_template openbsd arm
-build_template plan9 386
-build_template plan9 amd64
-build_template plan9 arm
+build_template openbsd arm64
+# build_template plan9 386
+# build_template plan9 amd64
+# build_template plan9 arm
 build_template solaris amd64
 build_template windows 386 .exe
 build_template windows amd64 .exe
+build_template windows arm .exe
